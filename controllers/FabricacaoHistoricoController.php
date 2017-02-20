088d206b56a359ba01211a8dd9d34f7899317319
bruno <brunofileh@gmail.com>
2017-02-19 23:51:04 -0300

    teste

--- controllers/FabricacaoHistoricoController.php
+++ controllers/FabricacaoHistoricoController.php
@@ -12,13 +12,12 @@ use yii\filters\VerbFilter;
 /**
  * FabricacaoHistoricoController implements the CRUD actions for FabricacaoHistorico model.
  */
-class FabricacaoHistoricoController extends Controller
-{
+class FabricacaoHistoricoController extends Controller {
+
     /**
      * @inheritdoc
      */
-    public function behaviors()
-    {
+    public function behaviors() {
         return [
             'verbs' => [
                 'class' => VerbFilter::className(),
@@ -33,14 +32,13 @@ class FabricacaoHistoricoController extends Controller
      * Lists all FabricacaoHistorico models.
      * @return mixed
      */
-    public function actionIndex()
-    {
+    public function actionIndex() {
         $searchModel = new FabricacaoHistoricoSearch();
         $dataProvider = $searchModel->search(Yii::$app->request->queryParams);
 
         return $this->render('index', [
-            'searchModel' => $searchModel,
-            'dataProvider' => $dataProvider,
+                    'searchModel' => $searchModel,
+                    'dataProvider' => $dataProvider,
         ]);
     }
 
@@ -49,10 +47,9 @@ class FabricacaoHistoricoController extends Controller
      * @param string $id
      * @return mixed
      */
-    public function actionView($id)
-    {
+    public function actionView($id) {
         return $this->render('view', [
-            'model' => $this->findModel($id),
+                    'model' => $this->findModel($id),
         ]);
     }
 
@@ -61,15 +58,14 @@ class FabricacaoHistoricoController extends Controller
      * If creation is successful, the browser will be redirected to the 'view' page.
      * @return mixed
      */
-    public function actionCreate()
-    {
+    public function actionCreate() {
         $model = new FabricacaoHistorico();
 
         if ($model->load(Yii::$app->request->post()) && $model->save()) {
             return $this->redirect(['view', 'id' => $model->id]);
         } else {
             return $this->render('create', [
-                'model' => $model,
+                        'model' => $model,
             ]);
         }
     }
@@ -80,15 +76,40 @@ class FabricacaoHistoricoController extends Controller
      * @param string $id
      * @return mixed
      */
-    public function actionUpdate($id)
-    {
+    public function actionUpdate($id) {
         $model = $this->findModel($id);
+        if (Yii::$app->request->post()) {
+            $post = Yii::$app->request->post();
+            if ($post['FabricacaoHistoricoSearch']['qnt'] == $model->qnt) {
+                $model->load($post);
+                $model->save();
+            } else {
+                $modelHitorico = new \app\models\FabricacaoHistoricoSearch();
+                $modelHitorico->load(Yii::$app->request->post());
+                $fabricacao = \app\models\FabricacaoSearch::findOne(['id' => $model->fabricacao_fk]);
 
-        if ($model->load(Yii::$app->request->post()) && $model->save()) {
-            return $this->redirect(['view', 'id' => $model->id]);
+                if ($post['FabricacaoHistoricoSearch']['qnt'] > $model->qnt) {
+
+                    $modelHitorico->qnt = ($post['FabricacaoHistoricoSearch']['qnt'] - $model->qnt);
+                    $modelHitorico->status = '1';
+                    $fabricacao->qnt = $fabricacao->qnt + $modelHitorico->qnt;
+                } else {
+
+                    $modelHitorico->qnt = ($model->qnt - $post['FabricacaoHistoricoSearch']['qnt']);
+                    $modelHitorico->status = '2';
+                    $fabricacao->qnt = $fabricacao->qnt - $modelHitorico->qnt;
+                }
+                $modelHitorico->fabricacao_fk = $model->fabricacao_fk;
+                $modelHitorico->pessoa = 'Ajuste Sistema';
+                $modelHitorico->save();
+                $model->load($post);
+                $fabricacao->save();
+                $model->save();
+            }
+            return $this->redirect(['fabricacao/view', 'id' => $model->fabricacao_fk]);
         } else {
             return $this->render('update', [
-                'model' => $model,
+                        'model' => $model,
             ]);
         }
     }
@@ -99,8 +120,7 @@ class FabricacaoHistoricoController extends Controller
      * @param string $id
      * @return mixed
      */
-    public function actionDelete($id)
-    {
+    public function actionDelete($id) {
         $this->findModel($id)->delete();
 
         return $this->redirect(['index']);
@@ -113,12 +133,12 @@ class FabricacaoHistoricoController extends Controller
      * @return FabricacaoHistorico the loaded model
      * @throws NotFoundHttpException if the model cannot be found
      */
-    protected function findModel($id)
-    {
-        if (($model = FabricacaoHistorico::findOne($id)) !== null) {
+    protected function findModel($id) {
+        if (($model = FabricacaoHistoricoSearch::findOne($id)) !== null) {
             return $model;
         } else {
             throw new NotFoundHttpException('The requested page does not exist.');
         }
     }
+
 }
